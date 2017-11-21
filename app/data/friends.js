053f4e83abef46808cdb51d14d4074cea5be5820
adamtre9115 <treadam9115@gmail.com>
2017-11-20 19:50:45 -0500

    move css add new logic for routing

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+// friend options
+var friends = [{
+        name: "ashley",
+        photo: "../images/ashley.jpg",
+        scores: [5, 3, 2, 2, 4, 1, 3, 3, 4, 5]
+    },
+    {
+        name: "justin",
+        photo: "../images/justin.jpg",
+        scores: [4, 3, 1, 4, 3, 1, 5, 4, 5, 3]
+    },
+    {
+        name: "laura",
+        photo: "../images/laura.jpg",
+        scores: [5, 4, 3, 4, 5, 2, 1, 3, 4, 3]
+    },
+    {
+        name: "monica",
+        photo: "../images/monica.jpg",
+        scores: [2, 3, 5, 4, 3, 2, 4, 5, 2, 5]
+    },
+    {
+        name: "ricardo",
+        photo: "../images/ricardo.jpg",
+        scores: [3, 2, 5, 4, 2, 4, 3, 4, 3, 5]
+    },
+    {
+        name: "richard",
+        photo: "../images/richard.jpg",
+        scores: [3, 2, 4, 2, 1, 5, 5, 3, 1, 2]
+    },
+    {
+        name: "staci",
+        photo: "../images/staci.jpg",
+        scores: [4, 3, 2, 4, 3, 3, 5, 4, 5, 3]
+    },
+    {
+        name: "stephanie",
+        photo: "../images/stephanie.jpg",
+        scores: [3, 2, 3, 4, 5, 2, 3, 4, 5, 4]
+    },
+    {
+        name: "steven",
+        photo: "../images/steven.jpg",
+        scores: [5, 1, 3, 2, 3, 2, 4, 2, 1, 5]
+    },
+    {
+        name: "timothy",
+        photo: "../images/timothy.jpg",
+        scores: [3, 4, 5, 3, 4, 1, 3, 5, 2, 3]
+    }
+]
