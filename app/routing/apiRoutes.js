d8dc98ed515f7f137c9df22633b269df208f1e29
adamtre9115 <treadam9115@gmail.com>
2017-11-22 19:53:39 -0500

    update server.js to send css update friend logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,6 +3,45 @@ var friendsData = require("../data/friends.js");
 module.exports = function (app) {
 
     app.get("/api/friends", function (req, res) {
-        res.json(friends);
+        res.json(friendsData);
+    });
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // ---------------------------------------------------------------------------
+
+    app.post('/api/friends', function (req, res) {
+        //grabs the new friend's scores to compare with friends in friendList array
+        var newFriendScores = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+
+        //runs through all current friends in list
+        for (var i = 0; i < friendsData.length; i++) {
+            var scoresDiff = 0;
+            //run through scores to compare friends
+            for (var j = 0; j < newFriendScores.length; j++) {
+                scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])));
+            }
+
+            //push results into scoresArray
+            scoresArray.push(scoresDiff);
+        }
+
+        //after all friends are compared, find best match
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        //return bestMatch data
+        var bff = friendsData[bestMatch];
+        res.json(bff);
+
+        //pushes new submission into the friendsList array
+        friendsData.push(req.body);
     });
 }
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 // Dependencies
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
+// Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3030;
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+app.use(express.static('app/public'));
+
+// Routes
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
-
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(bodyParser.json());
-app.use(express.static("app"));
-
 
 // set up listener
 app.listen(PORT, function () {
